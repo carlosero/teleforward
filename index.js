40ba8420dab996b6f9a972b4a52cf53738f4e697
Carlos Rodriguez <carlosr706@gmail.com>
2018-01-19 21:32:26 -0500

    Add procfile and 2 bots

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.TELEGRAM_TOKEN;
-var groupId // this will be set for group id
+var alertGroupId
+var newsGroupId
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const alertBot = new TelegramBot(process.env.TELEGRAM_INFORMATION_TOKEN, {polling: true});
+const newsBot = new TelegramBot(process.env.TELEGRAM_ALERT_TOKEN, {polling: true});
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-
-bot.onText(/test/, (msg, match) => {
+alertBot.onText(/test/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-  groupId = msg.chat.id
+  alertGroupId = msg.chat.id
   console.log("test", msg)
 });
 
 
-bot.on('message', (msg) => {
-	console.log("MSG", msg.chat.id, groupId, msg.message_id)
+alertBot.on('message', (msg) => {
+	console.log("MSG", msg.chat.id, alertGroupId, msg.message_id)
 	console.log(msg)
-	if (groupId && msg.chat.id && msg.message_id && msg.chat.id != groupId) {
-  		bot.forwardMessage(groupId, msg.chat.id, msg.message_id)
+	if (alertGroupId && msg.chat.id && msg.message_id && msg.chat.id != alertGroupId) {
+  		alertBot.forwardMessage(alertGroupId, msg.chat.id, msg.message_id)
+	}
+});
+
+newsBot.onText(/test/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  newsGroupId = msg.chat.id
+  console.log("test", msg)
+});
+
+
+newsBot.on('message', (msg) => {
+	console.log("MSG", msg.chat.id, newsGroupId, msg.message_id)
+	console.log(msg)
+	if (newsGroupId && msg.chat.id && msg.message_id && msg.chat.id != newsGroupId) {
+  		newsBot.forwardMessage(newsGroupId, msg.chat.id, msg.message_id)
 	}
 });
