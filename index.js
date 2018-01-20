d50565b1b0744037d13453a1c39d4d60768379fa
Carlos Rodriguez <carlosr706@gmail.com>
2018-01-19 23:04:54 -0500

    We only need one bot, actually

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ var ownerId = parseInt(process.env.TELEGRAM_OWNER_ID) // Owner's id who can do t
 
 
 // Create a bot that uses 'polling' to fetch new updates
-const newsBot = new TelegramBot(process.env.TELEGRAM_NEWS_TOKEN, {polling: true});
-const alertBot = new TelegramBot(process.env.TELEGRAM_ALERT_TOKEN, {polling: true});
+const petesBot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
 
 function addRedisId(group, id) {
 	if (groupIds[group].indexOf(id) == -1) {
@@ -36,58 +35,58 @@ client.get('forwardalerts', (err,c) => { groupIds['forwardalerts'] = parseIfExis
 client.get('forwardnews', (err,c) => { groupIds['forwardnews'] = parseIfExists(c) })
 
 // Receivers
-alertBot.onText(/\!enablealerts/, (msg, match) => {
+petesBot.onText(/\!enablealerts/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	addRedisId('alerts', msg.chat.id)
-  	alertBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
-newsBot.onText(/\!enablenews/, (msg, match) => {
+petesBot.onText(/\!enablenews/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	addRedisId('news', msg.chat.id)
-  	newsBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
-alertBot.onText(/\!disablealerts/, (msg, match) => {
+petesBot.onText(/\!disablealerts/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	removeRedisId('alerts', msg.chat.id)
-  	alertBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
-newsBot.onText(/\!disablenews/, (msg, match) => {
+petesBot.onText(/\!disablenews/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	removeRedisId('news', msg.chat.id)
-  	newsBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
 
 // Forwarders
-alertBot.onText(/\!forwardalerts/, (msg, match) => {
+petesBot.onText(/\!forwardalerts/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	addRedisId('forwardalerts', msg.chat.id)
-  	alertBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
-newsBot.onText(/\!forwardnews/, (msg, match) => {
+petesBot.onText(/\!forwardnews/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	addRedisId('forwardnews', msg.chat.id)
-  	newsBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
-alertBot.onText(/\!noforwardalerts/, (msg, match) => {
+petesBot.onText(/\!noforwardalerts/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	removeRedisId('forwardalerts', msg.chat.id)
-  	alertBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
-newsBot.onText(/\!noforwardnews/, (msg, match) => {
+petesBot.onText(/\!noforwardnews/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	removeRedisId('forwardnews', msg.chat.id)
-  	newsBot.sendMessage(msg.chat.id, 'Done.')
+  	petesBot.sendMessage(msg.chat.id, 'Done.')
   }
 });
 // Others
-alertBot.onText(/\!help/, (msg, match) => {
+petesBot.onText(/\!help/, (msg, match) => {
   if (msg.from.id == ownerId) {
   	message = "Commands:\n"
   	message += "!enablealerts: enable a channel to receive alerts\n"
@@ -99,26 +98,26 @@ alertBot.onText(/\!help/, (msg, match) => {
 	message += "!noforwardalerts: stop forwarding messages from this group\n"
 	message += "!noforwardnews: stop forwarding messages from this group\n"
 
-  	alertBot.sendMessage(msg.chat.id, message)
+  	petesBot.sendMessage(msg.chat.id, message)
   }
 })
 
 // Forward Handling
-alertBot.on('message', (msg) => {
+petesBot.on('message', (msg) => {
 	if (msg.chat.id && msg.message_id) {
 		if (groupIds['forwardalerts'].indexOf(msg.chat.id) != -1) {
 			groupIds['alerts'].forEach((id) => {
-  				alertBot.forwardMessage(id, msg.chat.id, msg.message_id)
+  				petesBot.forwardMessage(id, msg.chat.id, msg.message_id)
 			})
 		}
 	}
 });
 
-newsBot.on('message', (msg) => {
+petesBot.on('message', (msg) => {
 	if (msg.chat.id && msg.message_id) {
 		if (groupIds['forwardnews'].indexOf(msg.chat.id) != -1) {
 			groupIds['news'].forEach((id) => {
-  				alertBot.forwardMessage(id, msg.chat.id, msg.message_id)
+  				petesBot.forwardMessage(id, msg.chat.id, msg.message_id)
 			})
 		}
 	}
