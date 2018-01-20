0c8b064e441c92c4f99c7ee91c7d979e079b671b
Carlos Rodriguez <carlosr706@gmail.com>
2018-01-19 22:52:33 -0500

    Finish working on this

--- index.js
+++ index.js
@@ -1,43 +1,93 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+var redis = require("redis")
 
-var alertGroupId = process.env.TELEGRAM_ALERT_GROUP_ID
-var newsGroupId = process.env.TELEGRAM_INFORMATION_GROUP_ID
+// Setup
+var client = redis.createClient(process.env.REDIS_URL);
+var groupIds = {
+	'alerts': [],
+	'news': [],
+	'forwardalerts': [],
+	'forwardnews': []
+}
+
 
 // Create a bot that uses 'polling' to fetch new updates
-const alertBot = new TelegramBot(process.env.TELEGRAM_INFORMATION_TOKEN, {polling: true});
-const newsBot = new TelegramBot(process.env.TELEGRAM_ALERT_TOKEN, {polling: true});
+const newsBot = new TelegramBot(process.env.TELEGRAM_NEWS_TOKEN, {polling: true});
+const alertBot = new TelegramBot(process.env.TELEGRAM_ALERT_TOKEN, {polling: true});
 
-alertBot.onText(/test/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  alertGroupId = msg.chat.id
-  console.log("test", msg)
+function addRedisId(group, id) {
+	if (groupIds[group].indexOf(id) == -1) {
+		groupIds[group].push(id)
+		client.set(group, groupIds[group].join(','))
+	}
+}
+function removeRedisId(group, id) {
+	groupIds[group] = groupIds[group].filter((e) => e != id)
+	client.set(group, groupIds[group].join(','))
+}
+function parseIfExists(x) { return x ? x.split(',').map((e) => parseInt(e)) : []}
+
+client.get('alerts', (err,c) => { groupIds['alerts'] = parseIfExists(c) })
+client.get('news', (err,c) => { groupIds['news'] = parseIfExists(c) })
+client.get('forwardalerts', (err,c) => { groupIds['forwardalerts'] = parseIfExists(c) })
+client.get('forwardnews', (err,c) => { groupIds['forwardnews'] = parseIfExists(c) })
+
+// Receivers
+alertBot.onText(/\!enablealerts/, (msg, match) => {
+	console.log("HERE")
+  addRedisId('alerts', msg.chat.id)
+  alertBot.sendMessage(msg.chat.id, 'Done.')
+});
+newsBot.onText(/\!enablenews/, (msg, match) => {
+  addRedisId('news', msg.chat.id)
+  newsBot.sendMessage(msg.chat.id, 'Done.')
+});
+alertBot.onText(/\!disablealerts/, (msg, match) => {
+  removeRedisId('alerts', msg.chat.id)
+  alertBot.sendMessage(msg.chat.id, 'Done.')
+});
+newsBot.onText(/\!disablenews/, (msg, match) => {
+  removeRedisId('news', msg.chat.id)
+  newsBot.sendMessage(msg.chat.id, 'Done.')
+});
+
+// Forwarders
+alertBot.onText(/\!forwardalerts/, (msg, match) => {
+  addRedisId('forwardalerts', msg.chat.id)
+  alertBot.sendMessage(msg.chat.id, 'Done.')
+});
+newsBot.onText(/\!forwardnews/, (msg, match) => {
+  addRedisId('forwardnews', msg.chat.id)
+  newsBot.sendMessage(msg.chat.id, 'Done.')
+});
+alertBot.onText(/\!noforwardalerts/, (msg, match) => {
+  removeRedisId('forwardalerts', msg.chat.id)
+  alertBot.sendMessage(msg.chat.id, 'Done.')
+});
+newsBot.onText(/\!noforwardnews/, (msg, match) => {
+  removeRedisId('forwardnews', msg.chat.id)
+  newsBot.sendMessage(msg.chat.id, 'Done.')
 });
 
 
+// Forward Handling
 alertBot.on('message', (msg) => {
-	console.log("MSG", msg.chat.id, alertGroupId, msg.message_id)
-	console.log(msg)
-	if (alertGroupId && msg.chat.id && msg.message_id && msg.chat.id != alertGroupId) {
-  		alertBot.forwardMessage(alertGroupId, msg.chat.id, msg.message_id)
+	if (msg.chat.id && msg.message_id) {
+		if (groupIds['forwardalerts'].indexOf(msg.chat.id) != -1) {
+			groupIds['alerts'].forEach((id) => {
+  				alertBot.forwardMessage(id, msg.chat.id, msg.message_id)
+			})
+		}
 	}
 });
 
-newsBot.onText(/test/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  newsGroupId = msg.chat.id
-  console.log("test", msg)
-});
-
-
 newsBot.on('message', (msg) => {
-	console.log("MSG", msg.chat.id, newsGroupId, msg.message_id)
-	console.log(msg)
-	if (newsGroupId && msg.chat.id && msg.message_id && msg.chat.id != newsGroupId) {
-  		newsBot.forwardMessage(newsGroupId, msg.chat.id, msg.message_id)
+	if (msg.chat.id && msg.message_id) {
+		if (groupIds['forwardnews'].indexOf(msg.chat.id) != -1) {
+			groupIds['news'].forEach((id) => {
+  				alertBot.forwardMessage(id, msg.chat.id, msg.message_id)
+			})
+		}
 	}
 });
